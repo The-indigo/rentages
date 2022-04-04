f351fd82ed521ecdd4dc5eedd955cd76be5644de
The-indigo <adepojuadeyemi11@gmail.com>
2022-04-04 19:20:03 -0400

    configured server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/compurent'))
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname+'/dist/compurent/index.html'))
+})
+
+app.listen(process.env.port||8080)
